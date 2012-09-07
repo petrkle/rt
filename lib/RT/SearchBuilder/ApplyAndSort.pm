647f208e5e148d773d3ad276256001eacd51ba81
Ruslan Zakirov <ruz@bestpractical.com>
2012-09-07 04:21:20 +0400

    documentation for new ApplyAndSort API

--- lib/RT/SearchBuilder/ApplyAndSort.pm
+++ lib/RT/SearchBuilder/ApplyAndSort.pm
@@ -4,11 +4,21 @@ use warnings;
 package RT::SearchBuilder::ApplyAndSort;
 use base 'RT::SearchBuilder';
 
-sub RecordClass {
-    my $class = ref($_[0]) || $_[0];
-    $class =~ s/s$// or return undef;
-    return $class;
-}
+=head1 NAME
+
+RT::SearchBuilder::ApplyAndSort - base class for 'apply and sort' collections
+
+=head1 DESCRIPTION
+
+Base class for collections where records can be applied to objects with order.
+See also L<RT::Record::ApplyAndSort>. Used by L<RT::ObjectScrips> and
+L<RT::ObjectCustomFields>.
+
+As it's about sorting then collection is sorted by SortOrder field.
+
+=head1 METHODS
+
+=cut
 
 sub _Init {
     my $self = shift;
@@ -26,17 +36,53 @@ sub _Init {
     return $self->SUPER::_Init(@_);
 }
 
+=head2 RecordClass
+
+Returns class name of records in this collection. This generic implementation
+just strips trailing 's'.
+
+=cut
+
+sub RecordClass {
+    my $class = ref($_[0]) || $_[0];
+    $class =~ s/s$// or return undef;
+    return $class;
+}
+
+=head2 LimitToObjectId
+
+Takes id of an object and limits collection.
+
+=cut
+
 sub LimitToObjectId {
     my $self = shift;
     my $id = shift || 0;
     $self->Limit( FIELD => 'ObjectId', VALUE => $id );
 }
 
+=head2 NewItem
+
+Returns an empty new collection's item
+
+=cut
+
+sub NewItem {
+    my $self = shift;
+    return $self->RecordClass->new( $self->CurrentUser );
+}
+
+=head1 METHODS FOR TARGETS
+
+Rather than implementing a base class for targets (L<RT::Scrip>,
+L<RT::CustomField>) and its collections. This class provides
+class methods to limit target collections.
+
 =head2 LimitTargetToNotApplied
 
-Takes either list of object ids or nothing. Limits collection
-to custom fields to listed objects or any corespondingly. Use
-zero to mean global.
+Takes a collection object and optional list of object ids. Limits
+the collection to records not applied to listed objects or if
+the list is empty then any object. Use 0 (zero) to mean global.
 
 =cut
 
@@ -59,8 +105,8 @@ sub LimitTargetToNotApplied {
 
 =head2 LimitTargetToApplied
 
-Limits collection to custom fields to listed objects or any corespondingly. Use
-zero to mean global.
+L</LimitTargetToNotApplied> with reverse meaning. Takes the same
+arguments.
 
 =cut
 
@@ -80,6 +126,16 @@ sub LimitTargetToApplied {
     );
     return $alias;
 }
+
+=head2 JoinTargetToApplied
+
+Joins collection to this table using left join, limits joined table
+by ids if those are provided.
+
+Returns alias of the joined table. Join is cached and re-used for
+multiple calls.
+
+=cut
 
 sub JoinTargetToApplied {
     my $self = shift;
@@ -104,6 +160,15 @@ sub JoinTargetToApplied {
     return $alias;
 }
 
+=head3 JoinTargetToThis
+
+Joins target collection to this table using TargetField.
+
+Takes New and Left arguments. Use New to avoid caching and re-using
+this join. Use Left to create LEFT JOIN rather than inner.
+
+=cut
+
 sub JoinTargetToThis {
     my $self = shift;
     my $collection = shift;
@@ -123,17 +188,6 @@ sub JoinTargetToThis {
     );
     return $alias if $args{'New'};
     return $collection->{ $key } = $alias;
-}
-
-=head2 NewItem
-
-Returns an empty new collection's item
-
-=cut
-
-sub NewItem {
-    my $self = shift;
-    return $self->RecordClass->new( $self->CurrentUser );
 }
 
 RT::Base->_ImportOverlays();
